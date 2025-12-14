5b22d295c2517415d135a6377ecd979ce4251908
Sriram <sriramvonline@gmail.com>
2025-12-14 08:33:46 +0000

    1

--- aitest.py
+++ aitest.py
@@ -1,12 +1,22 @@
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
+from pathlib import Path
 
-load - load_dotenv('./../.env')
+# Load env variables
+load = load_dotenv("./../.env")
+FilePath = Path(__file__).parent / "secure_prompt.txt"
 
+with open(FilePath, "r", encoding="utf-8") as f:
+    secure_prompt = f.read().strip()
+
+# Initialize LLM with system prompt
 llm = ChatOllama(
-    model="gemini-3-pro-preview:latest", 
+    #model="llama3.2:3b",
+    model=gemini-3-pro-preview,
     base_url="http://localhost:11434",
     temperature=0.5,
     max_retries=3,
-    max_tokens=2048
+    max_tokens=2048,
+    system=secure_prompt
 )
+
